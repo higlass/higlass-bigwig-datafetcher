ec65c130ac9e25fda118423f03d15db3010bd86f
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-05-02 11:32:11 -0400

    Revert name change

--- src/BigwigDataFetcher.js
+++ src/BigwigDataFetcher.js
@@ -342,7 +342,7 @@ const BBIDataFetcher = function BBIDataFetcher(HGC, ...args) {
 }; // end function wrapper
 
 BBIDataFetcher.config = {
-  type: "bigwig",
+  type: "bbi",
 };
 
 export default BBIDataFetcher;
